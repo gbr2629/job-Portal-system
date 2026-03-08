cf598429b89ad98dc25e119910de6949fadac00d
Gauri Riswal <chmcs2026.gauri.riswal@gmail.com>
2026-03-09 01:27:59 +0530

    fix server.js and dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,10 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+dotenv.config()                         
 import connectDB from "./config/db.js"
 import userRoutes from "./routes/userRoutes.js"
 import jobRoutes from "./routes/jobRoutes.js"
-
-
-
-dotenv.config()
 
 const app = express()
 
@@ -20,11 +17,12 @@ app.use("/users", userRoutes)
 app.use("/jobs", jobRoutes)
 
 app.get("/", (req, res) => {
-  res.send("Job Portal API Running")
+    res.send("Job Portal API Running")
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-   console.log(`http://localhost:${PORT}`)
+    console.log(`http://localhost:${PORT}`)
 })
+
