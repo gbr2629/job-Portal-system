b8ae846049c3eec81c4fc92cbf331d7540d22853
Gauri Riswal <chmcs2026.gauri.riswal@gmail.com>
2026-03-14 21:59:09 +0530

    intial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./pages/user";
 import Jobs from "./pages/Jobs";
+import Register from "./pages/register";
+import Login from "./pages/login";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
         <Route path="/jobs" element={<Jobs />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
