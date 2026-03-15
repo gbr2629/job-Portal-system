75f0cb61e3c04ed07566c8a0b440cfda1bc9cb62
Gauri Riswal <chmcs2026.gauri.riswal@gmail.com>
2026-03-15 15:23:51 +0530

    intial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,21 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
-import connectDB from "./config/db.js";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import userRoutes from "./routes/userRoutes.js"
+import jobRoutes from "./routes/jobRoutes.js"
 
-const app = express();
 
+
+dotenv.config()
+
+const app = express()
 app.use(cors({
   origin: 'https://job-portal-system-1-p8gz.onrender.com',
   credentials: true
 }));
+
+app.use(express.json())
 
 connectDB()
 
@@ -17,12 +23,11 @@ app.use("/users", userRoutes)
 app.use("/jobs", jobRoutes)
 
 app.get("/", (req, res) => {
-    res.send("Job Portal API Running")
+  res.send("Job Portal API Running")
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+   console.log(`http://localhost:${PORT}`)
 })
-
